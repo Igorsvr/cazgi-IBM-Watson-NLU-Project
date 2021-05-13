121b4d2ce2ad3b92c09005b5fdc2478cfe945981
igorsvr <buldozerom@gmail.com>
2021-05-13 22:52:39 +0200

    Update App.js

--- sentimentAnalyzeClient/src/App.js
+++ sentimentAnalyzeClient/src/App.js
@@ -46,12 +46,12 @@ class App extends React.Component {
     ret = axios.get(url);
     ret.then((response)=>{
 
-      //Include code here to check the sentiment and fomrat the data accordingly
+      //Include code here to check the sentiment and format the data accordingly
 
       this.setState({sentimentOutput:response.data});
       let output = response.data;
       if(response.data === "positive") {
-        output = <div style={{color:"blue",fontSize:20}}>{response.data}</div>
+        output = <div style={{color:"lightblue",fontSize:20}}>{response.data}</div>
       } else if (response.data === "negative"){
         output = <div style={{color:"red",fontSize:20}}>{response.data}</div>
       } else {
